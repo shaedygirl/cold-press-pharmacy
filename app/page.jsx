5a5efedc629ab6cb721ae491a6c899432573f6c9
Shae Bardgett <shae@Shaes-MacBook-Pro-2.local>
2025-09-05 16:49:11 -0400

    chore: boot Next app

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:"2rem", fontFamily:"ui-sans-serif"}}>
+      <h1>Cold Press Pharmacy</h1>
+      <p>Nutrition you can feel. Data you can trust.</p>
+    </main>
+  );
+}
